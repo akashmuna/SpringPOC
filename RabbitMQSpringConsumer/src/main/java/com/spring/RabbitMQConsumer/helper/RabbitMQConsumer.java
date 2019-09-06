a64446c2d654904ac0a3fc18bc20eeac743d20f3
Akash_Mohapatra <akash.zlatan@gmail.com>
2019-09-06 15:19:58 +0530

    Adding Logger slf4j

--- RabbitMQSpringConsumer/src/main/java/com/spring/RabbitMQConsumer/helper/RabbitMQConsumer.java
+++ RabbitMQSpringConsumer/src/main/java/com/spring/RabbitMQConsumer/helper/RabbitMQConsumer.java
@@ -1,13 +1,17 @@
 package com.spring.RabbitMQConsumer.helper;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RabbitMQConsumer {
 
+	private final static Logger logger = LoggerFactory.getLogger(RabbitMQConsumer.class);
+	
 	@RabbitListener(queues = "${spring-boot-exchange.rabbitmq.queue}")
 	public void recievedMessage(String message) {
-		System.out.println("Recieved Message From RabbitMQ: " + message);
+		logger.info("Recieved Message From RabbitMQ: " + message);
 	}
 }
